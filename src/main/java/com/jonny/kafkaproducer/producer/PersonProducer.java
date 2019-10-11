2eb23bb4b33eb476a8cccefe471e79f0d09404eb
Jonathan roberts <burnleo23@gmail.com>
2019-10-11 17:06:47 -0500

    Update jackson library to 2.10.0 to fix vulnerability

--- src/main/java/com/jonny/kafkaproducer/producer/PersonProducer.java
+++ src/main/java/com/jonny/kafkaproducer/producer/PersonProducer.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Repository
+@Service
 public class PersonProducer {
 
   private static Logger LOG = LogManager.getLogger();
